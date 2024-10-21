241f3622b44b7075915208414f048be4a25b7d8d
dahiiv2 <danilnmellera@gmail.com>
2024-10-21 17:56:18 +0200

    10/21/2024 act13

---
+++ act13/index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>Creando la sesión</h1>
+
+    <form action="panelControl.php" method="post">
+        <label for="nombre">Nombres:</label>
+        <br> <br>
+        <input type="text" id="nombre" name="nombre" required></input>
+        <br> <br>
+        <button type="submit">Crear Sesión</button>
+    </form>
+</body>
+</html>
---
+++ act13/panelControl.php
@@ -0,0 +1,50 @@
+<?php
+//abrimos la sesion
+session_start();
+
+//miramos lo siguiente:
+//que sea un post
+//que se haya pasado nombre
+//y que no haya una sesion, asi si es la primera vez que entra asigna la variable a la sesion y pone sesionCreada a true
+if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST["nombre"]) && !isset($_SESSION["nombre"])) { 
+    $_SESSION["nombre"] = $_POST["nombre"]; 
+    $sesionCreada = true;
+    //si la sesion ya esta creada, miramos si coincide. si no lo hace, no permitimos acceso 
+} elseif (isset($_SESSION["nombre"])) { 
+    if ($_SESSION["nombre"] == $_POST["nombre"]) { 
+        $sesionCreada = true; 
+    } else { 
+        $sesionCreada = false; 
+    } 
+}
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <!-- lo que se muestra si se empieza sesion nueva o los nombres coinciden -->
+    <?php if ($sesionCreada) {?>
+        <h1>La sesion creada correctamente</h1>
+
+        <p>Bienvenido! Has iniciado sesion como: <strong><?php echo htmlspecialchars($_SESSION["nombre"])?></strong></p>
+
+        <a href="login.php">Ir a la página inicial</a>
+        <br>
+        <a href="CerrarSesion.php">Cerrar sesion</a>
+        <!-- lo que se muestra si los nombres no coinciden -->
+    <?php } else {?>
+        <h1>No ha iniciado sesion</h1>
+
+        <p>Acceso restringido, introduce el nombre correcto</p>
+
+        <a href="login.php">Ir a la página inicial</a>
+        <br>
+        <a href="CerrarSesion.php">Cerrar sesion</a>
+    <?php }?>
+</body>
+</html>
